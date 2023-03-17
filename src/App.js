5a83114c7bd6013538fe9ca84b22a04a91c998cb
Luckyhv <harshavardhan9680@gmail.com>
2023-03-17 20:02:29 +0530

    build-1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import './App.css'
+import useLocalstorage from './useLocalstorage'
 
 function App() {
+  const [html, sethtml] = useLocalstorage("html","")
+  const [css, setcss] = useLocalstorage("css","")
+  const [js, setjs] = useLocalstorage("js","")
+  const [doc, setdoc] = useState("")
+
+  useEffect(() => {
+    setdoc(`<html>
+        <body>${html}</body>
+        <style>${css}</style>
+        <script>${js}</script>
+      </html>`)
+  }, [html, css, js])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className="editors">
+        <div className="inputcon">
+          <label htmlFor="input">HTML</label>
+          <textarea className='input' value={html} onChange={(e) => { sethtml(e.target.value) }} />
+        </div>
+        <div className="inputcon">
+          <label htmlFor="input">CSS</label>
+          <textarea className='input' value={css} onChange={(e) => { setcss(e.target.value) }} />
+        </div>
+        <div className="inputcon">
+          <label htmlFor="input">Javascript</label>
+          <textarea className='input' value={js} onChange={(e) => { setjs(e.target.value) }} />
+        </div>
+      </div>
+      <div className="output">
+        <iframe srcDoc={doc} title="output" frameborder="0"></iframe>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
